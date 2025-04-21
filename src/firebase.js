0619beab8c81a437958b14b7ad3a6ae2a8bea3b4
John Marcelo <johntec.ads@gmail.com>
2025-04-21 13:53:30 -0300

    variáveis de ambiente

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
-// Substitua estas configurações pelas suas próprias do Firebase Console
+// Usando variáveis de ambiente para as configurações do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAnnqX-s98jDvwBz2mobBfraDzDTxYmFNY",
-  authDomain: "curriculos-a802a.firebaseapp.com",
-  projectId: "curriculos-a802a",
-  storageBucket: "curriculos-a802a.firebasestorage.app",
-  messagingSenderId: "329709244316",
-  appId: "1:329709244316:web:f9c25cd73b2de9b8990259",
-  measurementId: "G-FT5YJQ2NTL"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicializando Firebase
